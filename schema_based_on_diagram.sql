2d3e9f4ce575bae6176d4d510c40c4c83d33902b
AlucardSanin <alucardsanin@gmail.com>
2023-02-28 16:14:18 -0400

    Create table invoices & invoice items

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -13,3 +13,20 @@ CREATE TABLE medical_histories (
   status varchar(100) NOT NULL,
   PRIMARY KEY (id)
 );
+
+CREATE TABLE invoices (
+  id SERIAL,
+  total_amount DECIMAL(10,2) NOT NULL,
+  generated_at TIMESTAMP NOT NULL,
+  payed_at TIMESTAMP NOT NULL,
+  medical_history_id INT REFERENCES medical_histories(id) NOT NULL
+);
+
+CREATE TABLE invoice_items(
+  id SERIAL,
+  unit_price DECIMAL(10,2) NOT NULL,
+  quantity INT NOT NULL,
+  total_price DECIMAL(10,2),
+  invoice_id REFERENCES invoices(id) NOT NULL,
+  treatment_id REFERENCES treatments(id) NOT NULL
+);
