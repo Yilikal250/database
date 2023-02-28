698f5dec203cd4cd268e8f987490b1a37c56ec04
shinhyo26okazaki <shibo6937@gmail.com>
2023-02-28 16:28:33 -0400

    Add treatments and join table for medical histories and treatments

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -8,7 +8,7 @@ CREATE TABLE patients (
 CREATE TABLE medical_histories (
   id SERIAL,
   patient_id int NOT NULL,
-  addmited_at timestamp NOT NULL,
+  addmited_at timestamp DEFAULT CURRENT_TIMESTAMP NOT NULL,
   patient_id INT REFERENCES patients(id) NOT NULL,
   status varchar(100) NOT NULL,
   PRIMARY KEY (id)
@@ -17,9 +17,10 @@ CREATE TABLE medical_histories (
 CREATE TABLE invoices (
   id SERIAL,
   total_amount DECIMAL(10,2) NOT NULL,
-  generated_at TIMESTAMP NOT NULL,
-  payed_at TIMESTAMP NOT NULL,
-  medical_history_id INT REFERENCES medical_histories(id) NOT NULL
+  generated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+  payed_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+  medical_history_id INT REFERENCES medical_histories(id) NOT NULL,
+  PRIMARY KEY(id)
 );
 
 CREATE TABLE invoice_items(
@@ -28,5 +29,18 @@ CREATE TABLE invoice_items(
   quantity INT NOT NULL,
   total_price DECIMAL(10,2),
   invoice_id REFERENCES invoices(id) NOT NULL,
-  treatment_id REFERENCES treatments(id) NOT NULL
+  treatment_id REFERENCES treatments(id) NOT NULL,
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE treatments(
+  id SERIAL,
+  type VARCHAR(100),
+  name VARCHAR(100),
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE medical_histories_treatments(
+  medical_histories_id INT REFERENCES medical_histories(id) NOT NULL,
+  treatments_id INT REFERENCES treatments(id) NOT NULL
 );
